e58cfa23eee5dc5ac9b5a3d76434a8131a41a874
Rafael Ruiz-Clavijo <rruizclavijo@miuandes.cl>
2020-03-24 18:38:28 -0300

    Agregar la informacion de la clase cancion

---
+++ Reproductor Spotify/Reproductor Spotify/Espotifai.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reproductor_Spotify
+{
+    class Espotifai
+    {
+        private Cancion[] gestor;
+
+        public bool AgregarCancion(Cancion cancion)
+        {
+            int cant_canciones;
+            if (gestor == null)
+            {
+                cant_canciones = 0;
+            }
+            else
+            {
+                cant_canciones = gestor.Length;
+            }
+            string[] lista_info = {cancion.GetName(),cancion.GetAlbum(),cancion.GetArtist(),cancion.GetGenero()};
+            int verificador = 1;
+            for (int i = 0; i < cant_canciones; i++)
+            {
+                string[] comp_info = {gestor[i].GetName(), gestor[i].GetAlbum(), gestor[i].GetArtist(), gestor[i].GetGenero()};
+                if (comp_info == lista_info)
+                {
+                    verificador = 0;
+                }
+            }
+            if (verificador == 1)
+            {
+                gestor.append(cancion);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+    }
+}
---
+++ Reproductor Spotify/Reproductor Spotify/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reproductor_Spotify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cancion music = new Cancion("Cumbia del coronavirus","Pandemia","El Cumbias","Cumbia");
+            Espotifai g = new Espotifai();
+            Console.WriteLine(music.Informacion());
+            bool add1 = g.AgregarCancion(music);
+            Console.WriteLine(add1);
+            bool add2 = g.AgregarCancion(music);
+            Console.WriteLine(add2);
+        }
+    }
+}
