e6755b4e7557f2097949bca7a30e9faf43ed8f84
RafaRuiz-Clavijo <62115016+RafaRuiz-Clavijo@users.noreply.github.com>
2020-03-30 11:09:14 -0300

    Ver 1.1 -> Errores chicos corregidos + Bienvenida

---
+++ Playlist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reproductor_Spotify
+{
+    class Playlist
+    {
+        private string nameplaylist;
+        public Cancion[] listacancion = new Cancion[] { };
+
+        public Playlist(string n, Cancion[] l)
+        {
+            nameplaylist = n;
+            listacancion = l;
+
+        }
+
+        public string GetNamePlaylist()
+        {
+            return this.nameplaylist;
+        }
+
+    }
+}
